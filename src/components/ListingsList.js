f96529945934b7948eb1bd5e5bbb4ac0187b4752
dcruz354 <dcruz354@gmail.com>
2022-02-06 12:47:32 -0800

    display data in table

--- src/components/ListingsList.js
+++ src/components/ListingsList.js
@@ -7,6 +7,7 @@ const ListingsList = () => {
   const get_property_name = useRef(null);
 
   const [getResult, setGetResult] = useState(null);
+  const [getResult1, setGetResult1] = useState(null);
 
   const formatResponse = (res) => {
     return JSON.stringify(res, null, 2);
@@ -22,7 +23,7 @@ const ListingsList = () => {
       }
 
       const data = await res.json();
-
+/* 
       const result = {
         status: res.status + "-" + res.statusText,
         headers: {
@@ -31,9 +32,9 @@ const ListingsList = () => {
         },
         length: res.headers.get("Content-Length"),
         data: data,
-      };
+      }; */
 
-        setGetResult(formatResponse(result));
+        setGetResult(data);
       } catch (err) {
         setGetResult(err.message);
       }
@@ -41,6 +42,7 @@ const ListingsList = () => {
 
   const clearGetOutput = () => {
     setGetResult(null);
+    setGetResult1(null);
   }
 
   async function getListingByID() {
@@ -67,9 +69,9 @@ const ListingsList = () => {
           },
         };
 
-        setGetResult(formatResponse(result));
+        setGetResult1(formatResponse(result));
       } catch (err) {
-        setGetResult(err.message);
+        setGetResult1(err.message);
       }
     }
   }
@@ -98,9 +100,9 @@ const ListingsList = () => {
           },
         };
 
-        setGetResult(formatResponse(result));
+        setGetResult1(formatResponse(result));
       } catch (err) {
-        setGetResult(err.message);
+        setGetResult1(err.message);
       }
     }
   }
@@ -113,10 +115,11 @@ const ListingsList = () => {
       <div className='card-body'>
         <div className='input-group input-group-sm'>
           <button className='btn btn-sm btn-primary' onClick={getAllListings}>Get All Listings</button>
-
+          
           <input type="text" ref={get_id} className='form-control ml-2' placeholder='Id' />
           <div className='input-group-append'>
             <button className='btn btn-sm btn-primary' onClick={getListingByID}>Get Listing By Id</button>
+            
           </div>
 
           <input type="text" ref={get_property_name} className='form-control ml-2' placeholder='Property Name' />
@@ -126,7 +129,36 @@ const ListingsList = () => {
 
           <button className='btn btn-sm btn-warning ml-2' onClick={clearGetOutput}>Clear</button>
         </div>
-        { getResult && <div className='alert alert-secondary mt-2' role="alert"><pre>{getResult}</pre></div>}
+        { getResult1 && <div className='alert alert-secondary mt-2' role="alert"><pre>{getResult1}</pre></div>}
+        { getResult && 
+            <div>
+              <table className="table table-striped">
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Listing Url</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Summary</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Property Type</th>
+                    <th scope="col">Id</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {getResult.map(listing =>  {
+                    return (
+                      <tr>
+                          <th scope="row" key={listing.listingUrl}>{listing.listingUrl}</th>
+                          <td>{listing.name}</td>
+                          <td>{listing.summary}</td>
+                          <td>{listing.dexcription}</td>
+                          <td>{listing.propertyType}</td>
+                          <td>{listing.id}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>}
       </div>
     </div>
   );
